fe007a7782d516c0862acfce79cf2bf5507ac8e5
Almaz <almazbagmanov@yandex.ru>
2025-09-20 15:07:10 +0300

    Императивный подход от чистых корутин был переписан на реактивный с помощью flow/channel.

--- src/main/kotlin/dictionary/Display.kt
+++ src/main/kotlin/dictionary/Display.kt
@@ -1,35 +1,57 @@
 package dictionary
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.awt.BorderLayout
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 import java.util.concurrent.Executors
-import javax.swing.JButton
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.JScrollPane
-import javax.swing.JTextArea
-import javax.swing.JTextField
+import javax.swing.*
 
+@OptIn(FlowPreview::class)
 object Display {
 
     private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
+    private val queries = Channel<String>()
     val repository = Repository
 
+    init {
+        queries.consumeAsFlow()
+            .onEach {
+                searchButton.isEnabled = false
+                resultArea.text = "Loading"
+            }
+            .debounce(500)
+            .map { value ->
+                repository.loadData(value.trim())
+            }
+            .map {
+                it.joinToString("\n\n").ifEmpty { "Not Found" }
+            }
+            .onEach {
+                resultArea.text = it
+                searchButton.isEnabled = true
+            }
+            .launchIn(scope)
+    }
+
     private val enterWordLabel = JLabel("Enter word")
-    private val searchField = JTextField(20)
+    private val searchField = JTextField(20).apply {
+        addKeyListener(object : KeyAdapter() {
+            override fun keyReleased(e: KeyEvent?) {
+                loadDefinitions()
+            }
+        })
+    }
     private val searchButton = JButton("Search").apply {
         addActionListener {
-            scope.launch {
-                isEnabled = false
-                resultArea.text = "Loading"
-                resultArea.text = repository.loadData(searchField.text.trim()).joinToString("\n\n").ifEmpty { "Not Found" }
-                isEnabled = true
-            }
+            loadDefinitions()
         }
     }
     private val resultArea = JTextArea(25, 30).apply {
@@ -47,6 +69,10 @@ object Display {
         add(topPanel, BorderLayout.NORTH)
         add(JScrollPane(resultArea), BorderLayout.CENTER)
         pack()
+    }
+
+    private fun loadDefinitions() {
+        scope.launch { queries.send(searchField.text.trim()) }
     }
 
     fun show() {
