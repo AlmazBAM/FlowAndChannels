c5ef256092627900a0455b86a9a1b5392020decd
AlmazBag <almazbagmanov@yandex.ru>
2025-09-23 16:11:54 +0300

    Особенности наследования в корутинах: coroutineScope, supervisorScope

---
+++ src/main/kotlin/movies/Movies.kt
@@ -0,0 +1,44 @@
+package movies
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+import kotlin.time.measureTime
+
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val scope = CoroutineScope(dispatcher)
+
+fun main() {
+    val job = scope.launch {
+        val time = measureTime {
+            loadMovies()
+        }
+        println("Time $time")
+    }
+    scope.launch {
+        delay(4000)
+        println("Job cancelled")
+        job.cancel()
+    }
+}
+
+private suspend fun loadMoviesIds(): List<Int> {
+    delay(3000)
+    return (0..<100).toList()
+}
+
+private suspend fun loadMovieById(id: Int): String {
+    delay(100)
+    return "Movie $id"
+}
+
+private suspend fun loadMovies(): List<String> {
+    return coroutineScope {
+        loadMoviesIds().map {
+            async {
+                loadMovieById(it).also {
+                    println("Movie $it")
+                }
+            }
+        }.awaitAll()
+    }
+}
